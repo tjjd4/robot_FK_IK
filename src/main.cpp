3ac3f0b219fa65afe0f5a3317cd3ec24c6421723
tjjd4 <t01123581321345589@gmail.com>
2025-10-27 16:04:28 +0800

    feat: add pinocchio library

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,9 @@
+#include <pinocchio/fwd.hpp>
+#include <pinocchio/spatial/se3.hpp>
 #include <iostream>
-#include <Eigen/Dense>
 
 int main() {
-    Eigen::MatrixXd m(2,2);
-    m(0,0) = 3;
-    m(1,0) = 2.5;
-    m(0,1) = -1;
-    m(1,1) = m(1,0) + m(0,1);
-    std::cout << m << std::endl;
-    return 0;
+  pinocchio::SE3 T = pinocchio::SE3::Identity();
+  std::cout << T << std::endl;
+  return 0;
 }
